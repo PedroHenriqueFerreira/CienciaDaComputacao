88c73ec5b8eb341bb1d78c9db46dad9e38355fd1
Pedro Henrique Ferreira <63063916+PedroHenriqueFerreira@users.noreply.github.com>
2022-06-14 21:48:26 -0300

    Update ex016.py

--- FUP/ATV_FUP_5/ex016.py
+++ FUP/ATV_FUP_5/ex016.py
@@ -1,20 +1,12 @@
 def ordenacaoSelecao(l):
     lista = []
-    count = 0
     
     while len(l) > 0:
-        min = max = l[0]
+        min = l[0]
         for i in l:
             if i < min: min = i
-            elif i > max: max = i
         
-        lista.insert(count, min)
-        
-        if min != max:
-            lista.insert(count + 1, max)
-            l.remove(max)
-            
+        lista.append(min)
         l.remove(min)
-        count += 1 
     
     return lista
