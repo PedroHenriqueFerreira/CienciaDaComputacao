d36b237a7cf4458f610d26f2ba54dd0669d84c21
Pedro Henrique Ferreira <63063916+PedroHenriqueFerreira@users.noreply.github.com>
2022-07-27 20:54:34 -0300

    Add files via upload

---
+++ FUP/ATV_FUP_6/ex001.py
@@ -0,0 +1,3 @@
+horario = { 'hora': 3, 'minutos': 30, 'segundos': 15 }
+data = { 'dia': 22, 'mes': 'Abril', 'ano': 2018 }
+compromisso = { 'data': data, 'horario': horario, 'texto': 'Ir no banco sacar dinheiro' }
