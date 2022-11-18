4578327d13c63b97987e60982c788343085aeb8c
Pedro Henrique Ferreira <63063916+PedroHenriqueFerreira@users.noreply.github.com>
2022-11-18 13:44:14 -0300

    Delete FUP/ATV_FUP_6 directory

--- FUP/ATV_FUP_6/ex001.py
+++
@@ -1,3 +0,0 @@
-horario = { 'hora': 3, 'minutos': 30, 'segundos': 15 }
-data = { 'dia': 22, 'mes': 'Abril', 'ano': 2018 }
-compromisso = { 'data': data, 'horario': horario, 'texto': 'Ir no banco sacar dinheiro' }
